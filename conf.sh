7c46d4abf5160dcebe34bddbbbd5a5580af966ec
maher <mnstudios@hotmail.com>
2023-09-28 13:17:28 +0000

    update

--- conf.sh
+++ conf.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
+rm -rf ~/My-Git-Config 
 git clone https://github.com/maher-naija-pro/My-Git-Config.git  ~/My-Git-Config
+rm -rf ~/My-VimRC
 git clone https://github.com/maher-naija-pro/My-VimRC.git ~/My-VimRC
+rm -rf ~/My-zsh-conf
 git clone https://github.com/maher-naija-pro/My-zsh-conf.git ~/My-zsh-conf
+rm -rf ~/My_tmux_conf
 git clone https://github.com/maher-naija-pro/My_tmux_conf.git ~/My_tmux_conf
+rm -rf ~/My_ssh
 git clone https://github.com/maher-naija-pro/My-zsh-history.git ~/My_ssh 
+rm -rf ~/My_ssh
 git clone https://github.com/maher-naija-pro/My_ssh.git ~/My_ssh
+rm -rf  ~/My-aws-cli
 git clone https://github.com/maher-naija-pro/My-aws-cli.git  ~/My-aws-cli 
 
 rm -rf ~/.aws
@@ -21,10 +28,10 @@ rm -rf  ~/.gitconfig
 ln -s My-Git-Config/.gitconfig  ~/.gitconfig
 rm -rf ~/.ssh
 ln -s My_ssh  ~/.ssh 
-chmod -r 600  ~/.ssh/*.key
-chmod -r 600  ~/.ssh/*.rsa
+chmod 600  ~/.ssh/*.key
+chmod 600  ~/.ssh/*.rsa
 
-rm -rf  .zsh_history && ln -s My-zsh-history/.zsh_history   .zsh_history 
-
- 
-    
+rm -rf   ~/.zsh_history && ln -s My-zsh-history/.zsh_history    ~/.zsh_history 
+#
+# 
+#    
