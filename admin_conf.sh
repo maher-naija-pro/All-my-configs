803f27b2b5dbc0126956836ed9668d559336a381
maher <mnstudios@hotmail.com>
2023-09-29 18:57:52 +0200

    update

---
+++ admin_conf.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+sudo yum install -y git 
+
+
+if [[ ! -d ~/My-zsh-history   ]]
+then
+        git clone https://github.com/maher-naija-pro/My-zsh-history.git ~/My-zsh-history
+fi
+
+if [[ ! -d ~/My_ssh  ]]     
+then   
+        git clone https://github.com/maher-naija-pro/My_ssh.git  ~/My_ssh 
+fi
+
+if [[ ! -d ~/My-aws-cli   ]]     
+then
+          git clone  https://github.com/maher-naija-pro/My-aws-cli.git ~/My-aws-cli
+fi
+
+
+rm -rf ~/.aws
+ln -s ~/My-aws-cli  ~/.aws
+
+if [[ ! -f ~/.ssh/authorized_keys    ]]
+then
+  cp  ~/.ssh/authorized_keys /tmp
+fi
+mv  ~/.ssh ~/.ssh-old
+ln -s My_ssh  ~/.ssh
+cp /tmp/authorized_keys  ~/.ssh/
+chmod 600  ~/.ssh/*.key
+chmod 600  ~/.ssh/*.rsa
+
+rm -rf   ~/.zsh_history && ln -s My-zsh-history/.zsh_history    ~/.zsh_history 
